86c3951849bf6d0b6f8a2d5dd53fe661b09617cd
vukrosic <vuk.rosic@beam.ai>
2025-07-04 18:33:50 +0200

    cuda

--- train_gpt_single.py
+++ train_gpt_single.py
@@ -382,7 +382,7 @@ args = Hyperparameters()
 
 # Single GPU setup
 assert torch.cuda.is_available()
-device = torch.device("cuda")
+device = torch.device("cuda:0")
 torch.cuda.set_device(device)
 master_process = True
 
